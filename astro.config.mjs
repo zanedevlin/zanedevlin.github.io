2b9520179b224139bfcc05954d44eb6e4c42768d
dluman <dluman@allegheny.edu>
2026-02-04 15:35:49 -0500

    Updating with changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+    devToolbar: {
+        enabled: false
+    },
     integrations: [
         react(),
         mdx()
--- tests/portfolio.spec.ts
+++ tests/portfolio.spec.ts
@@ -13,12 +13,14 @@ test('has title', async ({ page }) => {
 test('has h1', async ({page}) => {
   await page.goto(process.env.BASE_URL);
   await expect(page.getByRole('heading', { name: 'Astro', level: 1 })).not.toBeVisible();
+  const h1 = page.locator('h1');
+  await expect(h1).not.toBeEmpty();
 });
 
 test('has nav', async ({page}) => {
   await page.goto(process.env.BASE_URL);
   const nav = page.locator('nav');
-  await expect(nav).toBeVisible();
+  await expect(nav).not.toBeEmpty();
 });
 
 test('has and follows links', async ({page}) => {
@@ -30,5 +32,41 @@ test('has and follows links', async ({page}) => {
 test('has footer', async ({page}) => {
   await page.goto(process.env.BASE_URL);
   const footer = page.locator('footer');
-  await expect(footer).toBeVisible();
+  await expect(footer).not.toBeEmpty();
+});
+
+test('has at least one section', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const section = page.locator('section');
+  await expect(section).not.toBeEmpty();
+});
+
+test('has at least one paragraph', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const para = page.locator('p');
+  await expect(para).not.toBeEmpty();
+});
+
+test('has a main element', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const main = page.locator('main');
+  await expect(main).not.toBeEmpty();
+});
+
+test('has an img element', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const img = page.getByRole('img');
+  await expect(img).not.toBeNull();
+});
+
+test('img element has src attribute', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const img = page.locator('img');
+  await expect(img).toHaveAttribute('src');
+});
+
+test('img element has alt attribute', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const img = page.locator('img');
+  await expect(img).toHaveAttribute('alt');
 });
